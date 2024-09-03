d80eccf6096c48518e3b27cd8c7de1b9464b8c9b
karan <karanche97@gmail.com>
2024-09-03 10:14:51 +0530

    [feat] added assets

--- apps/react-vite/src/components/seo/head.tsx
+++ apps/react-vite/src/components/seo/head.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Helmet, HelmetData } from 'react-helmet-async';
 
 type HeadProps = {
@@ -7,7 +8,22 @@ type HeadProps = {
 
 const helmetData = new HelmetData({});
 
+const getFaviconPath = (isDarkMode = false) => {
+  return `/favicon${isDarkMode ? '-dark' : ''}.ico`;
+};
+
 export const Head = ({ title = '', description = '' }: HeadProps = {}) => {
+  const [faviconHref, setFaviconHref] = useState('/favicon-light.png');
+
+  useEffect(() => {
+    // Get current color scheme.
+    const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+    // Set favicon initially.
+    setFaviconHref(getFaviconPath(matcher.matches));
+    // Change favicon if the color scheme changes.
+    matcher.onchange = () => setFaviconHref(getFaviconPath(matcher.matches));
+  }, [faviconHref]);
+
   return (
     <Helmet
       helmetData={helmetData}
@@ -15,6 +31,8 @@ export const Head = ({ title = '', description = '' }: HeadProps = {}) => {
       defaultTitle="Fieldproxy"
     >
       <meta name="description" content={description} />
+      <link rel="icon" href={faviconHref} />
+
     </Helmet>
   );
 };
